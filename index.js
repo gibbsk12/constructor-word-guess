f14992069a3703a2ea16eeda3952d443715ddf72
Kate Gibbs <kategibbs@Kates-MacBook-Pro-2.local>
2018-07-18 12:58:15 -0400

    records user letter

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+var Word = require("./Word.js");
 var prompt = require('prompt');
 
-prompt.start();
-prompt.get(['letter',], function (err, result) {
-  //
-  // Log the results.
-  //
-  console.log('Command-line input received:');
-  console.log('  username: ' + result.username);
-  console.log('  email: ' + result.email);
+//World Cup Qualifiers 
+var wordList = ["Russia", "Belgium", "Germany", "Iceland", "England", "Spain", "Poland", "Serbia", "France", "Portugal", "Switzerland", "Croatia", "Sweden", "Denmark", "Iran", "South Korea", "Japan", "Saudi Arabia", "Australia", "Nigeria", "Egypt", "Senegal", "Tunisia", "Morocco", "Mexico", "Costa Rica", "Panama", "Brazil", "Uruguary", "Argentina", "Colombia", "Peru"]
+
+prompt.get([{
+  name: 'letter',
+  description: 'What letter would you like to guess?',  
+  type: 'string',
+  required: true
+}], function (err, result) {
+  console.log('userGuess: ' + result.letter);
 });
