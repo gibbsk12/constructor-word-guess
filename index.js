fb70036578c44cd232a54387a557286f20eaf6b7
Kate Gibbs <kategibbs@Kates-MacBook-Pro-2.local>
2018-07-18 14:23:07 -0400

    list updated

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ var prompt = require('prompt');
 //World Cup Qualifiers 
 var wordList = ["Russia", "Belgium", "Germany", "Iceland", "England", "Spain", "Poland", "Serbia", "France", "Portugal", "Switzerland", "Croatia", "Sweden", "Denmark", "Iran", "South Korea", "Japan", "Saudi Arabia", "Australia", "Nigeria", "Egypt", "Senegal", "Tunisia", "Morocco", "Mexico", "Costa Rica", "Panama", "Brazil", "Uruguary", "Argentina", "Colombia", "Peru"]
 
+console.log("Welcome! Let's play hangman!");
+console.log("Each answer will be the name of a 2018 World Cup Qualifier");
+console.log("Bonne chance! Good luck! Alles Gute!");
+console.log("-----------------------------");
+prompt.start();
+
 prompt.get([{
   name: 'letter',
   description: 'What letter would you like to guess?',  
@@ -12,3 +18,4 @@ prompt.get([{
 }], function (err, result) {
   console.log('userGuess: ' + result.letter);
 });
+
