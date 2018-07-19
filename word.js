81c0b7024f7a621515dc519618929765f5d9d4c4
Kate Gibbs <kategibbs@Kates-MBP-2.fios-router.home>
2018-07-19 19:18:17 -0400

    new logic word file

--- word.js
+++ word.js
@@ -1,29 +1,52 @@
 var Letter = require("./Letter")
 
-var Word = function(word){
+var Word = function (word) {
     this.objectArray = [];
     this.displayArray = [];
-    for (var i = 0; i <word.length; i++){
+    this.underscores = 0;
+    this.guessesLeft = 10;
+    for (var i = 0; i < word.length; i++) {
         this.objectArray.push(new Letter(word.charAt(i)))
     };
-    this.print = function(){
+    this.print = function () {
         this.displayArray = [];
-        for (var i = 0; i < this.objectArray.length; i++){
+        for (var i = 0; i < this.objectArray.length; i++) {
             this.displayArray.push(this.objectArray[i].displayLetter())
+            if (this.arr[j].displayLetter() == "_") {
+                this.underscores++;
+            }
         }
         console.log(this.displayArray.join(" "));
     };
-    this.testLetter = function(x){
-        for (var i = 0; i < this.objectArray.length; i++){
+    this.testLetter = function (x) {
+        for (var i = 0; i < this.objectArray.length; i++) {
             this.objectArray[i].checkLetter(x);
+        };
+        for (var m = 0; m < this.arr.length; m++) {
+            if (this.arr[m].letter == x) {
+                found = true;
+                break;
+            };
+        };
+        if (found === false) {
+            this.guesses--;
+            console.log(`Incorrect!`)
+        } else if (found === true) {
+            console.log(`Correct!`)
         }
     };
-    this.noUnderscore = function(){
-        this.displayArray.every(this.didYouWin)
+    this.noUnderscore = function () {
+        if (this.arr2.every(this.didYouWin)) {
+            console.log(`You win!`);
+        } else {
+            if (this.guesses > 0) {
+                console.log(`Guess again!`);
+            }
+        }
     };
-    this.didYouWin = function(letter, index, array){
+    this.didYouWin = function (letter, index, array) {
         return letter != "_";
-    }
+    };
 
 }
 module.exports = Word;
