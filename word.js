87f2d33943a075c174b43a45fa19dd1f2c5891eb
Kate Gibbs <kategibbs@Kates-MacBook-Pro-2.local>
2018-07-20 09:19:18 -0400

    bug fix word

--- word.js
+++ word.js
@@ -1,52 +1,55 @@
-var Letter = require("./Letter")
+var Letter = require('./letter');
+
 
 var Word = function (word) {
     this.objectArray = [];
     this.displayArray = [];
     this.underscores = 0;
-    this.guessesLeft = 10;
+    this.guessesRemaining = 10;
     for (var i = 0; i < word.length; i++) {
         this.objectArray.push(new Letter(word.charAt(i)))
     };
     this.print = function () {
         this.displayArray = [];
+        this.underscores = 0;
         for (var i = 0; i < this.objectArray.length; i++) {
-            this.displayArray.push(this.objectArray[i].displayLetter())
-            if (this.objectArray[i].displayLetter() == "_") {
+            this.displayArray.push(this.objectArray[i].displayLetter());
+            if (this.objectArray[i].displayLetter() == '_') {
                 this.underscores++;
             }
         }
         console.log(this.displayArray.join(" "));
     };
     this.testLetter = function (x) {
+        var found = false;
         for (var i = 0; i < this.objectArray.length; i++) {
             this.objectArray[i].checkLetter(x);
-        };
-        for (var m = 0; m < this.arr.length; m++) {
-            if (this.arr[m].letter == x) {
+        }
+        for (var i = 0; i < this.objectArray.length; i++) {
+            if (this.objectArray[i].letter == x) {
                 found = true;
                 break;
             };
         };
         if (found === false) {
-            this.guesses--;
-            console.log(`Incorrect!`)
+            this.guessesRemaining--;
+            console.log('INCORRECT')
         } else if (found === true) {
-            console.log(`Correct!`)
+            console.log(`CORRECT`)
         }
     };
     this.noUnderscore = function () {
-        if (this.arr2.every(this.didYouWin)) {
-            console.log(`You win!`);
+        if (this.displayArray.every(this.didYouWin)) {
+            console.log(`YOU'VE WON`);
         } else {
-            if (this.guesses > 0) {
-                console.log(`Guess again!`);
-            }
+            if (this.guessesRemaining > 0) {
+            console.log('Guess again!');
+            } 
         }
     };
     this.didYouWin = function (letter, index, array) {
         return letter != "_";
     };
-
 }
+
 module.exports = Word;
